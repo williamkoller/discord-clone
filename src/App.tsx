4c3b3d897d9e6c3d2c6e33808aac1d698eb7a265
William Koller <wkoller25@gmail.com>
2020-06-18 00:34:57 -0300

    feat: add Layout components and GlobalStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from 'react'
 
+import Layout from './components/Layout'
+import GlobalStyles from './styles/GlobalStyles'
 
 function App() {
   return (
-    <h1>Discord Clone</h1>
-  );
+    <>
+      <Layout />
+      <GlobalStyles />
+    </>
+  )
 }
 
-export default App;
+export default App
