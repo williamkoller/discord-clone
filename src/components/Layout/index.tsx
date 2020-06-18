759ee555856c53038a179b606640901f0ce5c942
William Koller <wkoller25@gmail.com>
2020-06-18 08:15:25 -0300

    feat: add ServerName

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Grid } from './styles'
 
 import ServerList from '../ServerList'
+import ServerName from '../ServerName'
 const Layout: React.FC = () => {
   return (
     <Grid>
       <ServerList />
+      <ServerName />
     </Grid>
   )
 }
