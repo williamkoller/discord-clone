8e4764dbf102db8c5834728ece95d529eb42b985
William Koller <wkoller25@gmail.com>
2020-06-19 03:03:52 -0300

    feat: add Mention

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,10 +1,158 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import { Container, Messages, InputWrapper, Input, InputIcon } from './styles'
+import ChannelMessage, { Mention } from '../ChannelMessage'
 
 const ChannelData: React.FC = () => {
+  const messageRef = useRef() as React.MutableRefObject<HTMLDivElement>
+  useEffect(() => {
+    const div = messageRef.current
+    if (div) {
+      div.scrollTop = div.scrollHeight
+    }
+  }, [messageRef])
+
   return (
     <Container>
-      <Messages />
+      <Messages ref={messageRef}>
+        <ChannelMessage
+          author="William Koller"
+          date="21/06/2020"
+          content="Hoje é meu aniversário"
+        />
+        <ChannelMessage
+          author="William Koller"
+          date="21/06/2020"
+          content="Hoje é meu aniversário"
+        />
+        <ChannelMessage
+          author="William Koller"
+          date="21/06/2020"
+          content="Hoje é meu aniversário"
+        />
+        <ChannelMessage
+          author="William Koller"
+          date="21/06/2020"
+          content="Hoje é meu aniversário"
+        />
+        <ChannelMessage
+          author="William Koller"
+          date="21/06/2020"
+          content="Hoje é meu aniversário"
+        />
+        <ChannelMessage
+          author="William Koller"
+          date="21/06/2020"
+          content="Hoje é meu aniversário"
+        />
+        <ChannelMessage
+          author="William Koller"
+          date="21/06/2020"
+          content="Hoje é meu aniversário"
+        />
+        <ChannelMessage
+          author="William Koller"
+          date="21/06/2020"
+          content="Hoje é meu aniversário"
+        />
+        <ChannelMessage
+          author="William Koller"
+          date="21/06/2020"
+          content="Hoje é meu aniversário"
+        />
+        <ChannelMessage
+          author="William Koller"
+          date="21/06/2020"
+          content="Hoje é meu aniversário"
+        />
+        <ChannelMessage
+          author="William Koller"
+          date="21/06/2020"
+          content="Hoje é meu aniversário"
+        />
+        <ChannelMessage
+          author="William Koller"
+          date="21/06/2020"
+          content="Hoje é meu aniversário"
+        />
+        <ChannelMessage
+          author="William Koller"
+          date="21/06/2020"
+          content="Hoje é meu aniversário"
+        />
+        <ChannelMessage
+          author="William Koller"
+          date="21/06/2020"
+          content="Hoje é meu aniversário"
+        />
+        <ChannelMessage
+          author="William Koller"
+          date="21/06/2020"
+          content="Hoje é meu aniversário"
+        />
+        <ChannelMessage
+          author="William Koller"
+          date="21/06/2020"
+          content="Hoje é meu aniversário"
+        />
+        <ChannelMessage
+          author="William Koller"
+          date="21/06/2020"
+          content="Hoje é meu aniversário"
+        />
+        <ChannelMessage
+          author="William Koller"
+          date="21/06/2020"
+          content="Hoje é meu aniversário"
+        />
+        <ChannelMessage
+          author="William Koller"
+          date="21/06/2020"
+          content="Hoje é meu aniversário"
+        />
+        <ChannelMessage
+          author="William Koller"
+          date="21/06/2020"
+          content="Hoje é meu aniversário"
+        />
+        <ChannelMessage
+          author="William Koller"
+          date="21/06/2020"
+          content="Hoje é meu aniversário"
+        />
+        <ChannelMessage
+          author="William Koller"
+          date="21/06/2020"
+          content="Hoje é meu aniversário"
+        />
+        <ChannelMessage
+          author="William Koller"
+          date="21/06/2020"
+          content="Hoje é meu aniversário"
+        />
+        <ChannelMessage
+          author="William Koller"
+          date="21/06/2020"
+          content="Hoje é meu aniversário"
+        />
+        <ChannelMessage
+          author="William Koller"
+          date="21/06/2020"
+          content="Hoje é meu aniversário"
+        />
+
+        <ChannelMessage
+          author="William Koller"
+          date="21/06/2020"
+          content={
+            <>
+              <Mention>@William Koller</Mention>, me carrega no LOL e Cs de
+              novo, por favor?
+            </>
+          }
+          hasMention
+          isBot
+        />
+      </Messages>
 
       <InputWrapper>
         <Input type="text" placeholder="Conversarem #chat-livre" />
