9f23ed5252dd6ef75cf132e50fd051667903d8d4
William Koller <wkoller25@gmail.com>
2020-06-19 02:13:01 -0300

    feat: add component ChannelData

---
+++ src/components/ChannelData/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container, Messages, InputWrapper, Input, InputIcon } from './styles'
+
+const ChannelData: React.FC = () => {
+  return (
+    <Container>
+      <Messages />
+
+      <InputWrapper>
+        <Input type="text" placeholder="Conversarem #chat-livre" />
+        <InputIcon />
+      </InputWrapper>
+    </Container>
+  )
+}
+
+export default ChannelData
